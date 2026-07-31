4f3012d88744f71a04c8dbd08475530e57276e65
WooGeunHyeok <rmsgur0517@gmail.com>
2026-07-31 16:03:17 +0900

    Chore: batch 모듈 구조 추가 (MyBatis 연동)
    (cherry picked from commit 5e472198ca09d6fda9cf52322a9d8bc194222ad6)

--- batch/src/main/java/com/navi/loganalyzer/batch/BatchApplication.java
+++ batch/src/main/java/com/navi/loganalyzer/batch/BatchApplication.java
@@ -1,10 +1,14 @@
 package com.navi.loganalyzer.batch;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@MapperScan("com.navi.loganalyzer.batch.mapper")
 public class BatchApplication {
 
     public static void main(String[] args) {
@@ -12,7 +16,7 @@ public class BatchApplication {
                 .ignoreIfMissing()
                 .systemProperties()
                 .load();
+
         SpringApplication.run(BatchApplication.class, args);
     }
-
 }
